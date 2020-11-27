7be782d48cd366a537677ca119c40fd504068526
Maleina Bidek <maleina@ucla.edu>
2020-11-27 13:49:02 -0500

    feat: Add timer for each egg type

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,9 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    //let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
+    let eggTimes = ["Soft": 3, "Medium": 4, "Hard": 7]
+    
+    var timer = Timer()
+    
     @IBAction func hardnessSelected(_ sender: UIButton) {
-        print(sender.currentTitle as Any)
+        
+        timer.invalidate()
+        let hardness = sender.currentTitle!
+        var secondsRemaining = eggTimes[hardness]!
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { [self] (Timer) in
+            if secondsRemaining > 0 {
+                print ("\(secondsRemaining) seconds")
+                secondsRemaining -= 1
+            } else {
+                Timer.invalidate()
+                titleLabel.text = "Done!"
+            }
+            
+        }
+        
+        
     }
     
-
 }
