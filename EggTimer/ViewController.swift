8e25edbdf8f6627002e39a53ae91e0c6c691d8db
Maleina Bidek <maleina@ucla.edu>
2020-11-27 15:06:32 -0500

    feat: Display progress for each egg type and play alarm when done

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,31 +7,49 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var titleLabel: UILabel!
     
-    //let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
-    let eggTimes = ["Soft": 3, "Medium": 4, "Hard": 7]
-    
+    @IBOutlet weak var progressBar: UIProgressView!
+    let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
     var timer = Timer()
+    var totalTime = 0
+    var secondsPassed = 0
+    var player: AVAudioPlayer!
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
         
         timer.invalidate()
         let hardness = sender.currentTitle!
-        var secondsRemaining = eggTimes[hardness]!
+        let totalTime = eggTimes[hardness]!
+        var secondsPassed = 0
+        progressBar.progress = 0.0
+        titleLabel.text = hardness
+
         timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { [self] (Timer) in
-            if secondsRemaining > 0 {
-                print ("\(secondsRemaining) seconds")
-                secondsRemaining -= 1
+            
+            if secondsPassed < totalTime {
+                progressBar.progress = Float(secondsPassed)/Float(totalTime)
+                secondsPassed += 1
             } else {
                 Timer.invalidate()
+                progressBar.progress = 1.0
                 titleLabel.text = "Done!"
+                playSound()
             }
             
         }
+        
+        func playSound() {
+            let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+            player = try! AVAudioPlayer(contentsOf: url!)
+            player.play()
+                    
+        }
+            
         
         
     }
